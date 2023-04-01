02205fc5286d30fa0f00d6ca0b93f0aa6a13b9f9
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-04-02 02:16:34 +0530

    add profile photo and icons

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Typewriter from 'typewriter-effect';
+import { BsGithub, BsInstagram } from 'react-icons/bs';
+import { RxTwitterLogo } from 'react-icons/rx';
+import { SiGmail, SiMedium } from 'react-icons/si';
 
 import aboutContainer from '../../assets/aboutContainer.svg';
+import displayPicture from '../../assets/displayPicture.svg';
+import rotatingCircle from '../../assets/rotatingCircle.svg';
 import './LandingPage.css';
 
 const LandingPage = () => {
     return (
-        <div>
+        // <div>
         <div className="about">
-            <img src={aboutContainer} className='typing-container'/>
-            <div className="textarea">
+        <div className="textarea">
+            <div className='typewriter'> 
                 <Typewriter
                     onInit={(typewriter) => {
                     typewriter
@@ -30,18 +37,28 @@ const LandingPage = () => {
                         .typeString('... an App Dev enthusiast too ;)')
                         .pauseFor(1500)
                         .deleteAll()
-                        .typeString('I love wreading and writing blogs!')
+                        .typeString('I love reading and writing blogs!')
                         .pauseFor(2500)
                         .deleteAll()
                         .typeString("<span style='font-size: 1.8em; font-weight: 600'>Hello I'm Oindrila Das</span><br>")
                         .typeString("A web and app developer <br>")
-                        .typeString("Click on the contact button to connect with me <3")
                         .start();
                     }}
                 />
             </div>
+                <div className="socials">
+                    <Link className="social-icon"><RxTwitterLogo /></Link>
+                    <Link className="social-icon"><BsGithub /></Link>
+                    <Link className="social-icon"><SiGmail /></Link>
+                    <Link className="social-icon"><BsInstagram /></Link>
+                    <Link className="social-icon"><SiMedium /></Link>
+                </div>
+                <img src={aboutContainer} className='typing-container' style={{height: "15em"}}/>
+            </div>
+            <img src={displayPicture} className='display-picture' />
+            <img src={rotatingCircle} className='display-bg' />
         </div>
-        </div>
+        // </div>
     )
 }
 
